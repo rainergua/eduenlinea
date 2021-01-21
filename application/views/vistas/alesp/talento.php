e9f6047f3945478647ecf3165c188e9d9cb98eb4
rainergua <rainergua@gmail.com>
2021-01-21 11:59:24 -0400

    Adicion de breadcrumbs en alternativa y especial

--- application/views/vistas/alesp/talento.php
+++ application/views/vistas/alesp/talento.php
@@ -1,4 +1,11 @@
 <div class="container-fluid contenido">
+    <div class=" container d-flex justify-content-center py-2 my-2">
+        <ul class="pagination shadow-lg">
+            <li class="page-item"><a class="page-link" href="<?php echo base_url()?>"><i class="fa fa-home "></i> Inicio </a></li>
+            <li class="page-item"><a class="page-link " href="<?php echo base_url().'eduvit/alternativa'?>">Educación Alternativa y Especial</a></li>
+            <li class="page-item  active"><a class="page-link " href="<?php echo base_url().'conales/nivel/10'?>">Educación Especial</a></li>
+        </ul>
+    </div>
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-12 col-ld-12  text-center">
