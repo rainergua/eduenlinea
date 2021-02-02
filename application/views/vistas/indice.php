370c6df4e79f3a76777b04577fb603a84282a2fa
rainergua <rainergua@gmail.com>
2021-02-02 00:12:10 -0400

    Adicion de videos y contenidos

--- application/views/vistas/indice.php
+++ application/views/vistas/indice.php
@@ -102,74 +102,59 @@
         </div>
 
         <div class="row my-4 py-4">
-            <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
+                <div class="row my-1 py-1">
+                    <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+                        <img src="<?php echo base_url().'/assets/img/recursos/video.png';?>" class="img-fluid">
+                    </div>
+                    <div class="col-sm-12 col-md-8 col-ld-8 text-center">
+                        <div class="card-block">
+                            <h6 class="card-title">Videos Guía de uso educa.minedu.gob.bo</h6>
+                            <a href="<?php echo base_url().'/eduvit/videos';?>" class="btn btn-primary">Ver Videos</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
                 <div class="row my-1 py-1">
                     <div class="col-sm-12 col-md-4 col-ld-4 text-center">
                         <img src="<?php echo base_url().'/assets/img/recursos/001-reg.png';?>" class="img-fluid">
                     </div>
                     <div class="col-sm-12 col-md-8 col-ld-8 text-center">
                         <div class="card-block">
-                            <h5 class="card-title">Resolución Ministerial 001/2021 - Educación Regular</h5>
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Regular</h6>
                             <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4770&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
                 <div class="row my-1 py-1">
                     <div class="col-sm-12 col-md-4 col-ld-4 text-center">
                         <img src="<?php echo base_url().'/assets/img/recursos/001-esp.jpg';?>" class="img-fluid">
                     </div>
                     <div class="col-sm-12 col-md-8 col-ld-8 text-center">
                         <div class="card-block">
-                            <h5 class="card-title">Resolución Ministerial 001/2021 - Educación Alterativa y Especial</h5>
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Alterativa y Especial</h6>
                             <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4782&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
                 <div class="row my-1 py-1">
                     <div class="col-sm-12 col-md-4 col-ld-4 text-center">
                         <img src="<?php echo base_url().'/assets/img/recursos/001-sup.jpg';?>" class="img-fluid">
                     </div>
                     <div class="col-sm-12 col-md-8 col-ld-8 text-center">
                         <div class="card-block">
-                            <h5 class="card-title">Resolución Ministerial 001/2021 - Educación Superior y formación Profesional</h5>
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Superior y formación Profesional</h6>
                             <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4777&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <video src="<?php echo base_url().'/assets/img/video/prop.mp4';?>" controls></video>
-                <!--iframe class="embed-responsive-item" src=">" allowfullscreen></iframe-->
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
-                <div class="embed-responsive embed-responsive-16by9">
-                <video src="<?php echo base_url().'/assets/img/video/primer.mp4';?>" controls></video>
-                <!--iframe class="embed-responsive-item" src="<php echo base_url().'/assets/img/video/primer.mp4';?>" allowfullscreen></iframe-->
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
-                <div class="embed-responsive embed-responsive-16by9">
-                <video src="<?php echo base_url().'/assets/img/video/regu.mp4';?>" controls></video>
-                <!--iframe class="embed-responsive-item" src="<php echo base_url().'/assets/img/video/regu.mp4';?>" allowfullscreen></iframe-->
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
-                <div class="embed-responsive embed-responsive-16by9">
-                <video src="<?php echo base_url().'/assets/img/video/alter.mp4';?>" controls></video>
-                <!--iframe class="embed-responsive-item" src="<php echo base_url().'/assets/img/video/alter.mp4';?>" allowfullscreen></iframe-->
-                </div>
-            </div>
-        </div>
-
 
     </div>
 </div>
