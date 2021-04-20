8e465d33330fee9c8cd28dcbd6ec5cd65a8dafb2
rainergua <rainergua@gmail.com>
2021-04-20 00:24:14 -0400

    adicion de imagenes y Libros

---
+++ application/controllers/Libros.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Libros extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+		$this->load->model('otros_model');
+		$this->load->library('form_validation');
+		//$this->load->database('default');
+    }
+
+    public function index(){
+        $this->load->view('template/header');
+        $this->load->view('vistas/libros/libros');
+        $this->load->view('template/footer');
+    }
+    
+
+}
+?>
---
+++ application/views/vistas/libros/libros.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <div class="container mt-4 pt-4">
+        <div class="row">
+            <div class="col-sm-12 col-md-3 col-ld-3 ">
+                <figure class="figure">
+                    <img src="<?php echo base_url().'assets/pics/pag1.png'?>" class="figure-img img-fluid">
+                    <figcaption class="figure-caption text-center"><h6>
+                    Imagen</h6>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-sm-12 col-md-3 col-ld-3 ">
+                <figure class="figure">
+                    <img src="<?php echo base_url().'assets/pics/pag2.png'?>" class="figure-img img-fluid">
+                    <figcaption class="figure-caption text-center"><h6>
+                    Imagen</h6>
+                    </figcaption>
+                </figure>
+                
+            </div>
+        </div>
+
+    </div>
+</div>
