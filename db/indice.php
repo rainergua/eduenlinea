d2a321106d8d50d1ba4b24f3deca6a950f2239b9
rainergua <rainergua@gmail.com>
2021-06-21 11:39:56 -0400

    Adicion de apk

---
+++ db/indice.php
@@ -0,0 +1,215 @@
+<div class="container-fluid contenido">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <a href="<?php echo base_url().'libros';?>">
+                <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/0.png';?>" alt="Ir al Memoria del Mundo">
+                </a>
+            </div>
+            <div class="carousel-item">
+                <a href="<?php echo base_url().'educainnova';?>">
+                <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/1.png';?>" alt="Ir a Educa Innova">
+                </a>
+            </div>
+            <div class="carousel-item">
+            <a href="http://olimpiada.minedu.gob.bo/" target="_blank"><!--a href="https://dgtic.minedu.gob.bo/inscripcion/" target="_blank"-->
+            <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/2.png';?>" alt="Ir a la Olimpiada">
+            <!--div class="carousel-caption">
+                <h2>PARA MAESTROS</h2>
+                <p>Obten tu certificado respondiendo preguntas sencillas.</p>
+            </div-->
+            </a>
+            </div>
+            <div class="carousel-item">
+            <a href="<?php echo base_url().'tutoriales';?>" class="btn btn-primary">
+            <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/3.png';?>" alt="Ir a tutoriales">
+            </a>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/4.png';?>" alt="Educa televisión">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/5.jpg';?>" alt="Educa libros">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo base_url().'/assets/img/slide/6.jpg';?>" alt="2021 Año por la recuparación del derecho a la educación">
+            </div>
+        </div>
+        <!--a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a-->
+    </div>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Educa Bolivia</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url().'/assets/apps/ministerio4.apk';?>">Aplicación EducaBolivia</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url().'libros';?>">Memoria del Mundo Bolivia</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url().'educainnova';?>">Educa Innova</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://olimpiada.minedu.gob.bo/">10ma OCEPB</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url().'tutoriales';?>">Tutoriales para Maestros</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
+
+
+
+
+    <div class="container-fluid  mt-4 pt-4">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-ld-12  text-center">
+            <h2 class="text-center">2021 AÑO POR LA RECUPERACIÓN DEL DERECHO A LA EDUCACIÓN</h2>
+            </div>
+        </div>
+
+        <div class="row mt-4 pt-4">
+            <div class="col-sm-12 col-md-4 col-ld-4 text-justify">
+
+                <div class="card">
+                    <!--a href="<php echo base_url().'eduvit/regular';?>"></a-->
+                    <img src="<?php echo base_url().'/assets/img/btnreg.png';?>" class="card-img-top boton-sec" alt="Primaria">
+                    <div class="card-body">
+                        <h5 class="card-title">Regular</h5>
+                        <p class="card-text">Educación Inicial en Familia Comunitaria, Educación Primaria Comunitaria Vocacional y Educación Secundaria Comunitaria Productiva. 
+                            Es una biblioteca en línea para que puedas disfrutar del contenido elaborado para los estudiantes.</p>
+                            <div class="row">
+                            <div class="col-6"><a href="<?php echo base_url().'eduvit/regular';?>" class="btn btn-danger btn-block">Biblioteca</a></div>
+                            <div class="col-6"><a href="http://aula.minedu.gob.bo/regular" class="btn btn-info btn-block" target="_blank">Aula (Regular)</a></div>
+                            </div>
+                    </div>
+                </div>
+
+            </div>   
+            <div class="col-sm-12 col-md-4 col-ld-4 text-justify">
+                
+                <div class="card">
+                    <!--a href="<php echo base_url().'eduvit/alternativa';?>"></a-->
+                    <img src="<?php echo base_url().'/assets/img/btnalt.png';?>" class="card-img-top boton-sec" alt="Primaria">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Alternativa y Especial</h5>
+                        <p class="card-text">La Educación Alternativa y Especial es intracultural, intercultural y plurilingüe. Comprende ámbitos de Educación Alternativa y Educación Especial.</p>
+                        <div class="row">
+                            <div class="col-3"></div>
+                            <div class="col-6"><a href="<?php echo base_url().'eduvit/alternativa';?>" class="btn btn-primary btn-block">Biblioteca</a></div>
+                            <div class="col-3"></div>
+                        </div>
+                        <div class="row pt-1 mt-1">
+                            <div class="col-6"><a href="http://aula.minedu.gob.bo/alternativa" class="btn btn-warning text-white btn-block" target="_blank">Aula (Alternativa)</a></div>
+                            <div class="col-6"><a href="http://aula.minedu.gob.bo/especial" class="btn btn-secondary text-white btn-block" target="_blank">Aula (Especial)</a></div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-sm-12 col-md-4 col-ld-4 text-justify">
+                
+                <div class="card">
+                    <!--a href="<php echo base_url().'eduvit/superior';?>"></a-->
+                    <img src="<?php echo base_url().'/assets/img/btnsup.png';?>" class="card-img-top boton-sec" alt="Primaria">
+                    
+                    <div class="card-body">
+                        <h5 class="card-title">Superior</h5>
+                        <p class="card-text">Formación de Maestros Institutos Técnico Tecnológicos Universidades Indígenas Especiales y Privadas; 
+                        forman profesionales de alta competitividad con compromiso social y conciencia crítica al servicio del pueblo para el desarrollo del país. </p>
+                        <div class="row">
+                            <div class="col-6"><a href="<?php echo base_url().'eduvit/superior';?>" class="btn btn-success btn-block">Biblioteca</a></div>
+                            <div class="col-6"><a href="#" class="btn btn-info btn-block">Aula Superior</a></div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="row my-4 py-4">
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
+                <div class="row my-1 py-1">
+                    <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+                        <img src="<?php echo base_url().'/assets/img/recursos/video.png';?>" class="img-fluid">
+                    </div>
+                    <div class="col-sm-12 col-md-8 col-ld-8 text-center">
+                        <div class="card-block">
+                            <h6 class="card-title">Tutoriales</h6>
+                            <a href="<?php echo base_url().'tutoriales';?>" class="btn btn-primary btn-block">Estudiantes</a>
+                            <a href="https://dgtic.minedu.gob.bo/inscripcion/" class="btn btn-success btn-block" target="_blank">Maestros</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
+                <div class="row my-1 py-1">
+                    <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+                        <img src="<?php echo base_url().'/assets/img/recursos/001-reg.png';?>" class="img-fluid">
+                    </div>
+                    <div class="col-sm-12 col-md-8 col-ld-8 text-center">
+                        <div class="card-block">
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Regular</h6>
+                            <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4770&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
+                <div class="row my-1 py-1">
+                    <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+                        <img src="<?php echo base_url().'/assets/img/recursos/001-esp.jpg';?>" class="img-fluid">
+                    </div>
+                    <div class="col-sm-12 col-md-8 col-ld-8 text-center">
+                        <div class="card-block">
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Alterativa y Especial</h6>
+                            <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4782&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-3 col-ld-3 text-center">
+                <div class="row my-1 py-1">
+                    <div class="col-sm-12 col-md-4 col-ld-4 text-center">
+                        <img src="<?php echo base_url().'/assets/img/recursos/001-sup.jpg';?>" class="img-fluid">
+                    </div>
+                    <div class="col-sm-12 col-md-8 col-ld-8 text-center">
+                        <div class="card-block">
+                            <h6 class="card-title">Resolución Ministerial 001/2021 - Educación Superior y formación Profesional</h6>
+                            <a href="https://www.minedu.gob.bo/index.php?option=com_content&view=article&id=4777&catid=90&Itemid=933" class="btn btn-primary" target="_blank">Ver Resolución</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
