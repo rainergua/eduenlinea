0aba01b312dcc859bf9e93ba4c5e3169fd770adf
rainergua <rainergua@gmail.com>
2021-10-23 16:41:27 -0400

    Actualizacion Orden de Videos

--- application/views/vistas/ocepb/videos.php
+++ application/views/vistas/ocepb/videos.php
@@ -8,18 +8,6 @@
         <div class="row my-4">
             <div class="col-sm-12 col-md-12 col-ld-12">
                 <div class="card">
-                    <h5 class="card-header">Instalación del sistema sin internet - <a href="http://olimpiada.minedu.gob.bo/">10ma OCEPB</a></h5>
-                    <div class="card-body">
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <video src="<?php echo base_url().'/assets/img/video/ocepb/instsissi.mp4';?>" controls></video>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row my-4">
-            <div class="col-sm-12 col-md-12 col-ld-12">
-                <div class="card">
                     <h5 class="card-header">Modalidad de Prueba a Distancia (Con internet) - <a href="http://olimpiada.minedu.gob.bo/">10ma OCEPB</a></h5>
                     <div class="card-body">
                         <div class="embed-responsive embed-responsive-16by9">
@@ -36,6 +24,18 @@
                     <div class="card-body">
                         <div class="embed-responsive embed-responsive-16by9">
                             <video src="<?php echo base_url().'/assets/img/video/ocepb/modissi.mp4';?>" controls></video>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-4">
+            <div class="col-sm-12 col-md-12 col-ld-12">
+                <div class="card">
+                    <h5 class="card-header">Instalación del sistema sin internet - <a href="http://olimpiada.minedu.gob.bo/">10ma OCEPB</a></h5>
+                    <div class="card-body">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <video src="<?php echo base_url().'/assets/img/video/ocepb/instsissi.mp4';?>" controls></video>
                         </div>
                     </div>
                 </div>
